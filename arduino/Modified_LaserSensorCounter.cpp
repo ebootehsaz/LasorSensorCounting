94fcdde89cb896e80abf744af0754cae9f6a3f70
Ethan Bootehsaz <ethandb2024@gmail.com>
2023-11-30 17:04:28 -0500

    added arduino support

---
+++ arduino/Modified_LaserSensorCounter.cpp
@@ -0,0 +1,40 @@
+#include "LaserSensorCounter.h"
+
+LaserSensorCounter::LaserSensorCounter(ros::NodeHandle* nh, int pin) : sensor_pin(pin), counter(0), object_detected(false) {
+    Serial.begin(9600);
+
+    wiringPiSetupGpio(); // Use Broadcom pin numbering
+    pinMode(sensor_pin, INPUT);
+    pullUpDnControl(sensor_pin, PUD_UP); // Enable pull-up resistor
+    counter_pub = nh->advertise<std_msgs::Int32>("laser_sensor_counter", 10);
+}
+
+void LaserSensorCounter::checkSensor() {
+    if (digitalRead(sensor_pin) == LOW) { // LOW when laser is interrupted
+        if (!object_detected) {
+            counter++;
+            std_msgs::Int32 msg;
+            msg.data = counter;
+            counter_pub.publish(msg);
+    Serial.println(counter);
+        }
+        object_detected = true;
+    } else {
+        object_detected = false;
+    }
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "laser_sensor_counter");
+    ros::NodeHandle nh;
+    LaserSensorCounter lsc(&nh, 14); // Change the pin number accordingly
+
+    ros::Rate loop_rate(10); // Loop at 10Hz
+    while (ros::ok()) {
+        lsc.checkSensor();
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
